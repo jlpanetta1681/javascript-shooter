ed2cb55115fa3426d850e8366a8117d3056fb439
Joseph Panetta <jlpanetta1681@icloud.com>
2022-02-21 06:49:42 -0500

    working on projectiles

--- index.js
+++ index.js
@@ -20,10 +20,31 @@ class Player {
  }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+   }
+   draw() {
+    cntxt.beginPath()
+    cntxt.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    cntxt.fillStyle = this.color
+    cntxt.fill()
+}
+  }
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 const player = new Player(x, y, 30, 'blue')
 player.draw()
 
 
-console.log(player)
+
+addEventListener('click', (event) => {
+
+    const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
+    projectile.draw()   
+})
